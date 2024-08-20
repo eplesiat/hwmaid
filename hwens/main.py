3aba85d1bcd629c833a2006bb190da11bf1a37e0
Étienne Plésiat <plesiat@dkrz.de>
2024-08-20 17:35:41 +0200

    Optimize for large datasets

--- hwens/main.py
+++ hwens/main.py
@@ -1,6 +1,7 @@
 import argparse
 import xarray as xr
 from dask.diagnostics import ProgressBar
+from dask.distributed import Client
 
 def main():
     parser = argparse.ArgumentParser()
@@ -8,7 +9,11 @@ def main():
     parser.add_argument("-i", '--intensity', type=str, help='Intensity files', nargs="+")
     parser.add_argument("-c", '--cumintensity', type=str, help='Cumulative intensity files', nargs="+")
     parser.add_argument('-o', '--outname', type=str, default="", help='Output filename')
+    parser.add_argument('-n', '--n_chunks', type=int, default=10, help='Number of chunks in time')
     args = parser.parse_args()
+    
+    client = Client()
+    ProgressBar().register()
 
     outname = args.outname.split("/")
     if outname == [""]:
@@ -21,16 +26,22 @@ def main():
 
         print("* Treating", type)
         files = getattr(args, type)
-        ds = xr.open_mfdataset(files, combine='nested', concat_dim='member')
 
-        with ProgressBar():
-            ds.mean(dim="member").to_netcdf(dirname + type + rootname + "_mean.nc")
-            ds.std(dim="member").to_netcdf(dirname + type + rootname + "_std.nc")
+        
+        ds = xr.open_mfdataset(files, combine='nested', concat_dim='member', chunks={"time": args.n_chunks})
+        ds_ = ds.mean(dim="member")
+        ds_ = ds_.compute()
+        ds_.to_netcdf(dirname + type + rootname + "_mean.nc")
+        ds_ = ds.std(dim="member")
+        ds_ = ds_.compute()
+        ds_.to_netcdf(dirname + type + rootname + "_std.nc")
 
-    ds_prob = ~ds.isnull()
-    ds_prob = ds_prob.mean(dim="member")
+    print("* Calculating probability")
+    ds_ = ~ds.isnull()
+    ds_ = ds_.mean(dim="member")
+    ds_ = ds_.compute()
 
-    ds_prob.to_netcdf(dirname + type + rootname + "_prob.nc")
+    ds_.to_netcdf(dirname + type + rootname + "_prob.nc")
 
 if __name__ == "__main__":
     main()
